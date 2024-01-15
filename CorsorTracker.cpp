341bb7bb3bd751de796598a5b37d169dee5976a8
Patafix <gaspard.lebaube@gaskam.com>
2024-01-15 23:14:38 +0100

    Add Cm distance calculator : - Todo -> Binary GCD algorithm to auto find the aspect ratio

--- CorsorTracker.cpp
+++ CorsorTracker.cpp
@@ -5,12 +5,43 @@
 
 using namespace std;
 
+// Monitor Settings
+
+const float monitorDiagonalInches = 27;
+const int aspectRatioX = 16;
+const int aspectRatioY = 9;
+
 // Mouse Settings
 
-const int mouseHz = 1000;
-const int sleepTime = 1000 / mouseHz;
+const float mouseHz = 1000;
+const float sleepTime = 1000 / mouseHz;
+
+// Program Variables Initialization
+
+int numberOfMonitors = 0;
+int monitorPrimaryWidth = 0;
+int monitorPrimaryHeight = 0;
 
 int totalDistancePX = 0;
+int totalDistanceCM = 0;
+
+const float monitorDiagonalCM = monitorDiagonalInches * 2.54;
+const float monitorWidhtCM = monitorDiagonalCM * aspectRatioX / sqrt(pow(aspectRatioX, 2) + pow(aspectRatioY, 2));
+const float monitorHeightCM = monitorDiagonalCM * aspectRatioY / sqrt(pow(aspectRatioX, 2) + pow(aspectRatioY, 2));
+
+// const float pixelSizeCM = monitorWidhtCM / monitorPrimaryWidth;
+
+void monitorSizeAndCoordinates()
+{
+    RECT rcMonitor;
+    GetSystemMetrics(sizeof(rcMonitor));
+
+    numberOfMonitors = GetSystemMetrics(SM_CMONITORS);
+    monitorPrimaryWidth = GetSystemMetrics(SM_CXSCREEN);
+    monitorPrimaryHeight = GetSystemMetrics(SM_CYSCREEN);
+
+    cout << "Monitors: " << numberOfMonitors << "\n" << "Primary Monitor: " << monitorPrimaryWidth << "x" << monitorPrimaryHeight << endl;
+}
 
 void mouseDistanceBetweenCoordinates()
 {
@@ -33,7 +64,7 @@ void mouseDistanceBetweenCoordinates()
 
             totalDistancePX = totalDistancePX + distancePx;
             oldCursorPoint = cursorPoint;
-            
+
             Sleep(sleepTime);
         }
     }
@@ -46,12 +77,20 @@ void mouseDistanceBetweenCoordinates()
 int main()
 {
     cout << "Program Started!" << endl;
+
+    thread monitorInformation(monitorSizeAndCoordinates);
     thread mouseCapture(mouseDistanceBetweenCoordinates);
+
+    cout << "Monitor Diagonal: " << monitorDiagonalCM << "cm" << endl;
+    cout << "Monitor Width: " << monitorWidhtCM << "cm" << endl;
+    cout << "Monitor Height: " << monitorHeightCM << "cm" << endl;
+
     while (true)
     {
         Sleep(1000);
-        cout << "Total distance: " << totalDistancePX << "px" << endl;
+        cout << "Total distance: " << totalDistancePX << " px" << endl;
+        cout << "Total distance: " << totalDistancePX * (monitorWidhtCM / monitorPrimaryWidth) << " cm" << endl;
     }
-    
+
     return 0;
 }
